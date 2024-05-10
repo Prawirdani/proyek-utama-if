dd9897aac2a491b29bbcc3137289d5bda08c4ec5
Prawirdani <prawirdani@gmail.com>
2024-05-11 03:59:27 +0700

    adjust users table structure

--- server/database/migrations/000001_create_table_users.up.sql
+++ server/database/migrations/000001_create_table_users.up.sql
@@ -1,8 +1,11 @@
+CREATE TYPE Role AS ENUM ('Kasir', 'Manajer');
 CREATE TABLE users (
-	id VARCHAR(100) PRIMARY KEY,
-	name VARCHAR(100) NOT NULL,
-	email VARCHAR(50) NOT NULL UNIQUE,
+	id SERIAL PRIMARY KEY,
+	nama VARCHAR(100) NOT NULL,
+	username VARCHAR(100) UNIQUE NOT NULL,
 	password VARCHAR(255) NOT NULL,
+	aktif BOOL DEFAULT true,
+	role Role NOT NULL default 'Kasir',
 	created_at TIMESTAMPTZ DEFAULT current_timestamp,
 	updated_at TIMESTAMPTZ DEFAULT current_timestamp
 );
