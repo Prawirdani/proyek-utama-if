4121f46f0a8105cae6104fa0021d188241832c4c
Prawirdani <prawirdani@gmail.com>
2024-05-12 02:40:45 +0700

    fix wrong way to assign tipe pembayaran value

--- server/internal/valueobject/tipe_pembayaran.go
+++ server/internal/valueobject/tipe_pembayaran.go
@@ -18,12 +18,12 @@ var (
 	ErrorTipePembayaranInvalid = httputil.ErrBadRequest("Tipe pembayaran tidak valid!")
 )
 
-// Check if the tipe pembayaran is validm if valid assign the value to tp
+// Check if the tipe pembayaran is valid, if valid assign the value to tp
 func (tp *TipePembayaran) Valid(str string) error {
 	v := strings.ToUpper(str)
 	switch v {
 	case string(TipePembayaranCash), string(TipePembayaranCard), string(TipePembayaranMobile):
-		tp = (*TipePembayaran)(&v)
+		*tp = TipePembayaran(v)
 		return nil
 	}
 	return ErrorTipePembayaranInvalid
