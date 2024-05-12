eca244e30d60533a0d810871144eb750f4a5cfee
Prawirdani <prawirdani@gmail.com>
2024-05-12 21:45:00 +0700

    add pesanan basic func and test unit

--- server/internal/entity/pesanan.go
+++ server/internal/entity/pesanan.go
@@ -1,9 +1,9 @@
 package entity
 
 import (
-	"database/sql"
 	"time"
 
+	"github.com/prawirdani/golang-restapi/internal/model"
 	"github.com/prawirdani/golang-restapi/internal/valueobject"
 )
 
@@ -15,15 +15,51 @@ type Pesanan struct {
 	TipePesanan   valueobject.TipePesanan   `json:"tipe"`
 	StatusPesanan valueobject.StatusPesanan `json:"status"`
 	Detail        []DetailPesanan           `json:"detail"`
-	Pembayaran    *Pembayaran               `json:"pembayaran"`
-	Catatan       sql.NullString            `json:"catatan,omitempty"`
+	Catatan       *string                   `json:"catatan,omitempty"`
+	Total         int                       `json:"total"`
 	WaktuPesanan  time.Time                 `json:"waktuPesanan"`
 }
 
-func (p *Pesanan) CalculateTotal() int {
-	total := 0
-	for _, detail := range p.Detail {
-		total += detail.Subtotal
+func NewPesananDineIn(req model.PesananDineInRequest) Pesanan {
+	return Pesanan{
+		NamaPelanggan: req.NamaPelanggan,
+		Kasir: User{
+			ID: req.KasirID,
+		},
+		Meja: &Meja{
+			ID: req.MejaID,
+		},
+		TipePesanan:   valueobject.TipePesananDineIn,
+		StatusPesanan: valueobject.StatusPesananDiProses,
+		WaktuPesanan:  time.Now(),
 	}
-	return total
+}
+
+func NewPesananTakeAway(req model.PesananTakeAwayRequest) Pesanan {
+	return Pesanan{
+		NamaPelanggan: req.NamaPelanggan,
+		Kasir: User{
+			ID: req.KasirID,
+		},
+		TipePesanan:   valueobject.TipePesananTakeAway,
+		StatusPesanan: valueobject.StatusPesananDiProses,
+		WaktuPesanan:  time.Now(),
+	}
+}
+
+func (p *Pesanan) AddDetail(detail DetailPesanan) {
+	p.Detail = append(p.Detail, detail)
+	p.Total += detail.Subtotal
+}
+
+func (p *Pesanan) Selesaikan() {
+	p.StatusPesanan = valueobject.StatusPesananSelesai
+}
+
+func (p *Pesanan) Batalkan() {
+	p.StatusPesanan = valueobject.StatusPesananBatal
+}
+
+func (p *Pesanan) Sajikan() {
+	p.StatusPesanan = valueobject.StatusPesananDisajikan
 }
---
+++ server/internal/entity/pesanan_test.go
@@ -0,0 +1,105 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/prawirdani/golang-restapi/internal/model"
+	"github.com/prawirdani/golang-restapi/internal/valueobject"
+	"github.com/stretchr/testify/assert"
+)
+
+var meja = Meja{
+	ID:     1,
+	Nomor:  "M-1",
+	Status: valueobject.StatusMejaTersedia,
+}
+
+var menus = []Menu{
+	{
+		ID:    1,
+		Nama:  "Nasi Goreng",
+		Harga: 15000,
+	},
+	{
+		ID:    2,
+		Nama:  "Mie Goreng",
+		Harga: 12000,
+	},
+}
+
+var note = "Pesanan note"
+var detailRequest = []model.DetailPesananRequest{
+	{MenuID: menus[0].ID, Kuantitas: 2},
+	{MenuID: menus[1].ID, Kuantitas: 1},
+}
+var requestDineIn = model.PesananDineInRequest{
+	NamaPelanggan: "Prawirdani",
+	KasirID:       1,
+	MejaID:        1,
+	Catatan:       &note,
+}
+
+var requestTakeAway = model.PesananTakeAwayRequest{
+	NamaPelanggan: "Prawirdani",
+	KasirID:       1,
+	Catatan:       &note,
+}
+
+func TestNewPesananDineIn(t *testing.T) {
+	t.Run("NewPesananDineIn", func(t *testing.T) {
+		pesanan := NewPesananDineIn(requestDineIn)
+
+		assert.Equal(t, requestDineIn.NamaPelanggan, pesanan.NamaPelanggan)
+		assert.Equal(t, requestDineIn.KasirID, pesanan.Kasir.ID)
+		assert.Equal(t, requestDineIn.MejaID, pesanan.Meja.ID)
+		assert.Equal(t, valueobject.TipePesananDineIn, pesanan.TipePesanan)
+		assert.Equal(t, valueobject.StatusPesananDiProses, pesanan.StatusPesanan)
+		assert.NotEmpty(t, pesanan.WaktuPesanan)
+	})
+
+	t.Run("NewPesananDineIn-AddDetail", func(t *testing.T) {
+		pesanan := NewPesananDineIn(requestDineIn)
+
+		expectedTotal := 0
+		for i := 0; i < len(detailRequest); i++ {
+			menu := menus[i]
+			detail, err := NewDetailPesanan(menu, detailRequest[i].Kuantitas)
+			expectedTotal += detail.Subtotal
+			assert.Nil(t, err)
+			pesanan.AddDetail(detail)
+
+		}
+
+		assert.NotZero(t, pesanan.Total)
+		assert.Equal(t, expectedTotal, pesanan.Total)
+	})
+}
+
+func TestNewPesananTakeAway(t *testing.T) {
+	t.Run("NewPesananTakeAway", func(t *testing.T) {
+		pesanan := NewPesananTakeAway(requestTakeAway)
+
+		assert.Equal(t, requestTakeAway.NamaPelanggan, pesanan.NamaPelanggan)
+		assert.Equal(t, requestTakeAway.KasirID, pesanan.Kasir.ID)
+		assert.Nil(t, pesanan.Meja)
+		assert.Equal(t, valueobject.TipePesananTakeAway, pesanan.TipePesanan)
+		assert.Equal(t, valueobject.StatusPesananDiProses, pesanan.StatusPesanan)
+		assert.NotEmpty(t, pesanan.WaktuPesanan)
+	})
+
+	t.Run("NewPesananTakeAway-AddDetail", func(t *testing.T) {
+		pesanan := NewPesananTakeAway(requestTakeAway)
+
+		expectedTotal := 0
+		for i := 0; i < len(detailRequest); i++ {
+			menu := menus[i]
+			detail, err := NewDetailPesanan(menu, detailRequest[i].Kuantitas)
+			expectedTotal += detail.Subtotal
+			assert.Nil(t, err)
+			pesanan.AddDetail(detail)
+		}
+
+		assert.NotZero(t, pesanan.Total)
+		assert.Equal(t, expectedTotal, pesanan.Total)
+	})
+}
