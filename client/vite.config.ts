ce4afb29f94bb5f2d35940db977d307109aad190
Prawirdani <prawirdani@gmail.com>
2024-05-14 05:59:00 +0700

    change proxy url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -12,5 +12,11 @@ export default defineConfig({
   },
   server: {
     port: 6969,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:42069',
+        changeOrigin: true,
+      },
+    },
   },
 });
