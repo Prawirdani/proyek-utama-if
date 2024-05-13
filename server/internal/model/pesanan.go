aaf3f39ec036b2abd207a3ab209a7eef63d0ba9d
Prawirdani <prawirdani@gmail.com>
2024-05-13 23:56:11 +0700

    add pesanan response model

--- server/internal/model/pesanan.go
+++ server/internal/model/pesanan.go
@@ -1,5 +1,7 @@
 package model
 
+import "time"
+
 type PesananMenuRequest struct {
 	MenuID    int `json:"menuID" validate:"required"`
 	Kuantitas int `json:"kuantitas" validate:"required,min=1"`
@@ -18,4 +20,25 @@ type PesananTakeAwayRequest struct {
 	Menu          []PesananMenuRequest `json:"menu" validate:"required"`
 	Catatan       *string              `json:"catatan"`
 	KasirID       int
+}
+
+type PesananResponse struct {
+	ID            int                     `json:"id"`
+	NamaPelanggan string                  `json:"namaPelanggan"`
+	Kasir         string                  `json:"kasir"`
+	Meja          string                  `json:"meja"`
+	Tipe          string                  `json:"tipe"`
+	Status        string                  `json:"status"`
+	Catatan       *string                 `json:"catatan"`
+	Detail        []PesananDetailResponse `json:"detail"`
+	Total         int                     `json:"total"`
+	WaktuPesanan  time.Time               `json:"waktuPesanan"`
+}
+
+type PesananDetailResponse struct {
+	ID        int    `json:"id"`
+	NamaMenu  string `json:"namaMenu"`
+	HargaMenu int    `json:"hargaMenu"`
+	Kuantitas int    `json:"kuantitas"`
+	Subtotal  int    `json:"subtotal"`
 }
