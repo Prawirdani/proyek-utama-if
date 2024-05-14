ba3bfeae585056e61bd67367b2d46582218fccd1
Prawirdani <prawirdani@gmail.com>
2024-05-15 05:53:24 +0700

    refactor layouting mechanism

--- client/src/layout/Dashboard.tsx
+++ client/src/layout/Dashboard.tsx
@@ -5,27 +5,27 @@ import { Navigate, Outlet } from 'react-router-dom';
 import { useAuth } from '@/context/useAuth';
 
 export default function Dashboard() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-  const { isAuthenticated } = useAuth();
+	const [sidebarOpen, setSidebarOpen] = useState(false);
+	const { isAuthenticated } = useAuth();
 
-  return isAuthenticated ? (
-    <div className="flex min-h-screen overflow-hidden">
-      {/* Sidebar component */}
-      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-      {/* Sidebar component */}
+	return isAuthenticated ? (
+		<div className="flex min-h-screen overflow-hidden">
+			{/* Sidebar component */}
+			<Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+			{/* Sidebar component */}
 
-      <div className="relative flex-1 flex flex-col overflow-y-auto overflow-x-hidden">
-        {/* Header component */}
-        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        {/* Header component */}
-        {/* Main content */}
-        <main className="bg-secondary h-full p-8">
-          <Outlet />
-        </main>
-        {/* Main content */}
-      </div>
-    </div>
-  ) : (
-    <Navigate to="/login" replace />
-  );
+			<div className="flex-1 flex flex-col overflow-y-auto overflow-x-hidden pl-0 lg:pl-72 pt-12 lg:pt-18">
+				{/* Header component */}
+				<Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+				{/* Header component */}
+				{/* Main content */}
+				<main className="bg-secondary h-full p-8 pt-10 lg:pt-14">
+					<Outlet />
+				</main>
+				{/* Main content */}
+			</div>
+		</div>
+	) : (
+		<Navigate to="/login" replace />
+	);
 }
