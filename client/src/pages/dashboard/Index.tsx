d6e0c439db8f29fec34b3a79fc239d2b40d6b5bf
Prawirdani <prawirdani@gmail.com>
2024-05-14 18:57:39 +0700

    add dashboard index page

---
+++ client/src/pages/dashboard/Index.tsx
@@ -0,0 +1,12 @@
+import PageTitle from '@/components/pageTitle';
+
+export default function Index() {
+  return (
+    <>
+      <PageTitle title="Dashboard" />
+      <div>
+        <h1>Index Dashboard</h1>
+      </div>
+    </>
+  );
+}
