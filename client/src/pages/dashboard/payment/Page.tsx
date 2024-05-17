4b9a33f5cdf3ad6b5655aa248bf1f3624726a248
Prawirdani <prawirdani@gmail.com>
2024-05-17 16:15:27 +0700

    fix mismatch window title

--- client/src/pages/dashboard/payment/Page.tsx
+++ client/src/pages/dashboard/payment/Page.tsx
@@ -12,66 +12,66 @@ import Delete from './Delete';
 import { SquarePen, Trash } from 'lucide-react';
 
 export default function Page() {
-  const { loading, payment_methods } = usePaymentMethods();
-  const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
-  const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
-  const [updateTarget, setUpdateTarget] = useState<MetodePembayaran>({} as MetodePembayaran);
+	const { loading, payment_methods } = usePaymentMethods();
+	const [openUpdateDialog, setOpenUpdateDialog] = useState(false);
+	const [openDeleteDialog, setOpenDeleteDialog] = useState(false);
+	const [updateTarget, setUpdateTarget] = useState<MetodePembayaran>({} as MetodePembayaran);
 
-  const triggerUpdateForm = (m: MetodePembayaran) => {
-    setUpdateTarget(m);
-    setOpenUpdateDialog(true);
-  };
+	const triggerUpdateForm = (m: MetodePembayaran) => {
+		setUpdateTarget(m);
+		setOpenUpdateDialog(true);
+	};
 
-  const triggerDeleteForm = (m: MetodePembayaran) => {
-    setUpdateTarget(m);
-    setOpenDeleteDialog(true);
-  };
+	const triggerDeleteForm = (m: MetodePembayaran) => {
+		setUpdateTarget(m);
+		setOpenDeleteDialog(true);
+	};
 
-  return loading ? (
-    <Loader />
-  ) : (
-    <section>
-      <TitleSetter title="Menu" />
-      <div className="-space-y-1 mb-4">
-        <H2>Pembayaran</H2>
-        <p>Manajemen metode pembayaran</p>
-      </div>
+	return loading ? (
+		<Loader />
+	) : (
+		<section>
+			<TitleSetter title="Pembayaran" />
+			<div className="-space-y-1 mb-4">
+				<H2>Pembayaran</H2>
+				<p>Manajemen metode pembayaran</p>
+			</div>
 
-      <div className="flex justify-end mb-4">
-        <PaymentMethodAddForm />
-        {/* Update Form, Triggered by each row edit button */}
-        <PaymentMethodUpdateForm updateTarget={updateTarget} open={openUpdateDialog} setOpen={setOpenUpdateDialog} />
-        <Delete id={updateTarget.id} open={openDeleteDialog} setOpen={setOpenDeleteDialog} />
-      </div>
-      <Card className="p-8">
-        <Table>
-          <TableHeader>
-            <TableRow className="[&>th]:text-medium">
-              <TableHead>Nama Metode</TableHead>
-              <TableHead>Tipe Pembayaran</TableHead>
-              <TableHead>Deskripsi</TableHead>
-              <TableHead className="w-[10%]"></TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {payment_methods?.map((m) => (
-              <TableRow key={m.id}>
-                <TableCell>{m.metode}</TableCell>
-                <TableCell>{m.tipePembayaran}</TableCell>
-                <TableCell className="max-w-lg">{m.deskripsi}</TableCell>
-                <TableCell className="w-fit flex gap-4 [&>button]:shadow-md [&>button]:w-12 [&>button]:p-0">
-                  <Button onClick={() => triggerUpdateForm(m)} variant="outline">
-                    <SquarePen className="h-4 w-4" />
-                  </Button>
-                  <Button onClick={() => triggerDeleteForm(m)} variant="destructive">
-                    <Trash className="h-4 w-4" />
-                  </Button>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </Card>
-    </section>
-  );
+			<div className="flex justify-end mb-4">
+				<PaymentMethodAddForm />
+				{/* Update Form, Triggered by each row edit button */}
+				<PaymentMethodUpdateForm updateTarget={updateTarget} open={openUpdateDialog} setOpen={setOpenUpdateDialog} />
+				<Delete id={updateTarget.id} open={openDeleteDialog} setOpen={setOpenDeleteDialog} />
+			</div>
+			<Card className="p-8">
+				<Table>
+					<TableHeader>
+						<TableRow className="[&>th]:text-medium">
+							<TableHead>Nama Metode</TableHead>
+							<TableHead>Tipe Pembayaran</TableHead>
+							<TableHead>Deskripsi</TableHead>
+							<TableHead className="w-[10%]"></TableHead>
+						</TableRow>
+					</TableHeader>
+					<TableBody>
+						{payment_methods?.map((m) => (
+							<TableRow key={m.id}>
+								<TableCell>{m.metode}</TableCell>
+								<TableCell>{m.tipePembayaran}</TableCell>
+								<TableCell className="max-w-lg">{m.deskripsi}</TableCell>
+								<TableCell className="w-fit flex gap-4 [&>button]:shadow-md [&>button]:w-12 [&>button]:p-0">
+									<Button onClick={() => triggerUpdateForm(m)} variant="outline">
+										<SquarePen className="h-4 w-4" />
+									</Button>
+									<Button onClick={() => triggerDeleteForm(m)} variant="destructive">
+										<Trash className="h-4 w-4" />
+									</Button>
+								</TableCell>
+							</TableRow>
+						))}
+					</TableBody>
+				</Table>
+			</Card>
+		</section>
+	);
 }
