66286aa7f3d7daff6867a9f42d8bf1f4f0d387a8
Prawirdani <prawirdani@gmail.com>
2024-05-30 05:06:18 +0700

    update: change json struct tag

--- server/internal/entity/pembayaran.go
+++ server/internal/entity/pembayaran.go
@@ -4,10 +4,10 @@ import "time"
 
 type Pembayaran struct {
 	Id              int              `json:"id"`
-	PesananId       int              `json:"pesanan_id,omitempty"`
+	PesananId       int              `json:"pesananId,omitempty"`
 	Metode          MetodePembayaran `json:"metode"`
 	Jumlah          int              `json:"jumlah"`
-	WaktuPembayaran time.Time        `json:"waktu_pembayaran"`
+	WaktuPembayaran time.Time        `json:"waktuPembayaran"`
 }
 
 func NewPembayaran(pesanan Pesanan, metode MetodePembayaran) *Pembayaran {
