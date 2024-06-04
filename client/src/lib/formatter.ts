55f85bcda44887d78ba6ab4e09a27929dabd96fb
Prawirdani <prawirdani@gmail.com>
2024-06-04 21:06:31 +0700

    add formatter & titlecase util

---
+++ client/src/lib/formatter.ts
@@ -0,0 +1,25 @@
+export function formatIDR(number: number): string {
+  return new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR' }).format(number);
+}
+
+export function formatDateTime(date: Date): string {
+  const formatter = new Intl.DateTimeFormat('id-ID', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    hour12: false,
+    timeZoneName: 'short',
+  });
+  return formatter.format(date);
+}
+
+export function formatTime(date: Date): string {
+  const formatter = new Intl.DateTimeFormat('id-ID', {
+    year: 'numeric',
+    month: 'short',
+    day: '2-digit',
+  });
+  return formatter.format(date);
+}
